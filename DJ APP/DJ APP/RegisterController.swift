e084bec3f97f5c470b85347ddd0bce15745485e9
Micajuine Ho <ho.micajuine@gmail.com>
2017-09-01 20:55:01 -0400

    Register Controller: Add photo and hometown
    added button onto the uiview of the pic,  for add photo
    created the add hometown view

--- DJ APP/DJ APP/RegisterController.swift
+++ DJ APP/DJ APP/RegisterController.swift
@@ -10,44 +10,62 @@ import UIKit
 
 class RegisterController: UIViewController {
 
-//    let profilePic: UIImageView = {
-//        let pp = UIImageView()
-//        pp.image = UIImage(named: "usernameIcon")
-//        pp.contentMode = .scaleAspectFit
-//        pp.layer.cornerRadius = 30
-//        pp.clipsToBounds = true
-//        pp.layer.borderWidth = 1.0
-//        pp.layer.borderColor = UIColor.black.cgColor
-//        pp.translatesAutoresizingMaskIntoConstraints = false
-//        return pp
-//    }()
-    
-    let profilePicButton: UIButton = {
-        let ppb = UIButton(type: UIButtonType.custom)
-        let image = UIImage(named: "usernameIcon")
-        ppb.addTarget(self, action: #selector(handlePicTapped), for: .touchUpInside)
-        ppb.setImage(image, for: .normal)
-        ppb.translatesAutoresizingMaskIntoConstraints = false
-        return ppb
+    let profilePic: UIImageView = {
+        let pp = UIImageView()
+        pp.alpha = 0.85
+        pp.image = UIImage(named: "usernameIcon")
+        pp.contentMode = .scaleAspectFit
+        pp.layer.cornerRadius = 60
+        pp.clipsToBounds = true
+        pp.layer.borderWidth = 1.0
+        pp.layer.borderColor = UIColor.black.cgColor
+        pp.translatesAutoresizingMaskIntoConstraints = false
+        return pp
     }()
     
-    let cancelButton: UIBarButtonItem = {
-        let cb = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
-        
-        return cb
+    let addPhoto: UIButton = {
+        let ap = UIButton()
+        let lightblue = UIColor(colorLiteralRed: 94/255, green: 211/255, blue: 237/255, alpha: 1)
+        ap.setTitleColor(lightblue, for: .normal)
+        ap.setTitleColor(UIColor.white, for: .highlighted)
+        ap.titleLabel?.lineBreakMode = .byWordWrapping
+        ap.titleLabel?.textAlignment = .center
+        ap.titleLabel?.font = ap.titleLabel?.font.withSize(30)
+        ap.setTitle("Add\nphoto", for: .normal)
+        ap.translatesAutoresizingMaskIntoConstraints = false
+        ap.addTarget(self, action: #selector(handlePicTapped), for: .touchUpInside)
+        return ap
     }()
+    
+    let hometownLabel: UILabel = {
+        let ht = UILabel()
+        ht.textColor = UIColor.gray
+        ht.text = "Hometown:"
+        ht.backgroundColor = UIColor.red
+        ht.translatesAutoresizingMaskIntoConstraints = false
+        return ht
+    }()
+    
+    let hometownTextField: UITextField = {
+        let htf = UITextField()
+        htf.backgroundColor = UIColor.yellow
+        htf.clearButtonMode = .whileEditing
+        htf.translatesAutoresizingMaskIntoConstraints = false
+        return htf
+    }()
+    
+    let hometownSep: UIView = {
+        let hs = UIView()
+        hs.backgroundColor = UIColor.lightGray
+        hs.translatesAutoresizingMaskIntoConstraints = false
+        return hs
+    }()
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let backgroundImage: UIImageView = UIImageView(frame: view.bounds)
-//        backgroundImage.image = UIImage(named: "djBackgroundImage")
-//        backgroundImage.contentMode = .scaleAspectFill
-//        view.insertSubview(backgroundImage, at: 0)
-
         view.backgroundColor = UIColor.white
         
-       
         setupNavigationBar()
         setupViews()
     }
@@ -58,12 +76,40 @@ class RegisterController: UIViewController {
     }
     
     func setupViews(){
-        view.addSubview(profilePicButton)
+        view.addSubview(profilePic)
+        view.addSubview(addPhoto)
+        view.addSubview(hometownLabel)
+        view.addSubview(hometownTextField)
+        view.addSubview(hometownSep)
+
         
-        profilePicButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        profilePicButton.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 25).isActive = true
-        profilePicButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        profilePicButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        profilePic.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profilePic.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 24).isActive = true
+        profilePic.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        profilePic.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        
+        addPhoto.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        addPhoto.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 25).isActive = true
+        addPhoto.widthAnchor.constraint(equalTo: profilePic.widthAnchor, multiplier: 1).isActive = true
+        addPhoto.heightAnchor.constraint(equalTo: profilePic.heightAnchor, multiplier: 1).isActive = true
+        
+        hometownLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive = true
+        hometownLabel.topAnchor.constraint(equalTo: profilePic.bottomAnchor, constant: 24).isActive = true
+        hometownLabel.widthAnchor.constraint(equalToConstant: hometownLabel.intrinsicContentSize.width).isActive = true
+        hometownLabel.heightAnchor.constraint(equalToConstant: 36).isActive = true
+        
+        hometownTextField.leftAnchor.constraint(equalTo: hometownLabel.rightAnchor, constant: 12).isActive = true
+        hometownTextField.topAnchor.constraint(equalTo: hometownLabel.topAnchor).isActive = true
+        hometownTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive = true
+        hometownTextField.heightAnchor.constraint(equalTo: hometownLabel.heightAnchor).isActive = true
+        
+        hometownSep.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        hometownSep.topAnchor.constraint(equalTo: hometownLabel.bottomAnchor, constant: 6).isActive = true
+        hometownSep.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        hometownSep.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        
     }
     
     func handlePicTapped() {
