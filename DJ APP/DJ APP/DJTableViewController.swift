5e26ae59983aa012c476789286f044689a1a0046
Micajuine Ho <ho.micajuine@gmail.com>
2017-09-20 20:30:04 -0400

    Image Boarder and Background Gradient to each cell

--- DJ APP/DJ APP/DJTableViewController.swift
+++ DJ APP/DJ APP/DJTableViewController.swift
@@ -17,7 +17,9 @@ class DJTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
-        
+        tableView.backgroundColor = UIColor.lightGray
+        //remove seperators from empty cells
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
         tableView.register(DJCell.self, forCellReuseIdentifier: cellId)
         
         fetchDjs()
@@ -92,19 +94,15 @@ class DJTableViewController: UITableViewController {
     func handleLogout() {
         dismiss(animated: true, completion: nil)
     }
+
    
 }
 
 class DJCell: UITableViewCell {
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        //10 for left and right, 50 for size of image
-        textLabel?.frame = CGRect(x: 70, y: textLabel!.frame.origin.y - 3, width: textLabel!.frame.width, height: textLabel!.frame.height)
-        detailTextLabel?.frame = CGRect(x: 70, y: detailTextLabel!.frame.origin.y + 1
-            , width: detailTextLabel!.frame.width, height: textLabel!.frame.height)
-    }
-    
+    let gradientLayer = CAGradientLayer()
+    let firstColor: CGColor = UIColor(red: 0, green: 0.832, blue: 0.557, alpha: 1.0).cgColor
+    let secondColor: CGColor = UIColor(red: 0, green: 0.635, blue: 0.923, alpha: 1.0).cgColor
+   
     let profileImageView: UIImageView = {
        let iv = UIImageView()
         iv.image = UIImage(named: "usernameIcon")
@@ -112,12 +110,36 @@ class DJCell: UITableViewCell {
         iv.translatesAutoresizingMaskIntoConstraints = false
         iv.layer.masksToBounds = true
         iv.layer.cornerRadius = 25
+        iv.layer.borderColor = UIColor.black.cgColor
+        iv.layer.borderWidth = 2
         return iv
     }()
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //10 for left and right, 50 for size of image
+        textLabel?.frame = CGRect(x: 70, y: textLabel!.frame.origin.y - 3, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        textLabel?.backgroundColor = UIColor.clear
+        detailTextLabel?.frame = CGRect(x: 70, y: detailTextLabel!.frame.origin.y + 1
+            , width: detailTextLabel!.frame.width, height: textLabel!.frame.height)
+        detailTextLabel?.backgroundColor = UIColor.clear
+    }
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+   
+        gradientLayer.colors = [firstColor, secondColor]
+        gradientLayer.locations = [0, 0.5]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.frame = self.layer.bounds
+        
+        layer.insertSublayer(gradientLayer, at: 0)
         
         setupViews()
+        
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -131,6 +153,8 @@ class DJCell: UITableViewCell {
         profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         profileImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
         profileImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+   
     }
-    
 }
+
+
