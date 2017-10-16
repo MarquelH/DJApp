f7191a3176bd3c27cf62f340eb2a31cc13a477fa
MarquelH <marquelhendricks96@gmail.com>
2017-10-15 22:40:34 -0400

    Changes 10/15

--- DJ APP/DJ APP/TrackCell.swift
+++ DJ APP/DJ APP/TrackCell.swift
@@ -14,7 +14,6 @@ class TrackCell: UITableViewCell {
         setupViews()
     }
     
-
     let separator: UIView = {
         let s = UIView()
         s.translatesAutoresizingMaskIntoConstraints = false
@@ -22,58 +21,20 @@ class TrackCell: UITableViewCell {
         return s
     }()
     
-
-    let darkView: UIView = {
-        let dk = UIView()
-        dk.backgroundColor = UIColor.darkGray
-        return dk
-    }()
-    
-    let separator: UIView = {
-        let s = UIView()
-        s.translatesAutoresizingMaskIntoConstraints = false
-        s.backgroundColor = UIColor.white
-        return s
-    }()
-    
-    lazy var gradientLayer: CAGradientLayer = {
-        let gl = CAGradientLayer()
-        let firstColor: CGColor = UIColor(red: 0, green: 0.832, blue: 0.557, alpha: 1.0).cgColor
-        let secondColor: CGColor = UIColor(red: 0, green: 0.635, blue: 0.923, alpha: 1.0).cgColor
-        gl.colors = [firstColor, secondColor]
-        gl.locations = [0, 0.5]
-        gl.startPoint = CGPoint(x: 0, y: 0)
-        gl.endPoint = CGPoint(x: 0, y: 1)
-        return gl
-    }()
-    
-
     override func layoutSubviews() {
         super.layoutSubviews()
         //10 for left and right, 50 for size of image
         textLabel?.frame = CGRect(x: 5, y: textLabel!.frame.origin.y - 3, width: textLabel!.frame.width, height: textLabel!.frame.height)
-
         //textLabel?.backgroundColor = UIColor.clear
         detailTextLabel?.frame = CGRect(x: 5, y: detailTextLabel!.frame.origin.y + 1
             , width: detailTextLabel!.frame.width, height: textLabel!.frame.height)
         //detailTextLabel?.backgroundColor = UIColor.clear
         
         // darkView.frame = contentView.frame
-
-        textLabel?.backgroundColor = UIColor.clear
-        detailTextLabel?.frame = CGRect(x: 5, y: detailTextLabel!.frame.origin.y + 1
-           , width: detailTextLabel!.frame.width, height: textLabel!.frame.height)
-        detailTextLabel?.backgroundColor = UIColor.clear
-        //contentView.layer.insertSublayer(gradientLayer, at: 0)
-        //contentView.backgroundColor = UIColor(red: 214/255, green: 29/255, blue: 1, alpha:1.0)
-        //gradientLayer.frame = contentView.frame
-        darkView.frame = contentView.frame
-
         
     }
     
     
-
     //    func cellClicked() {
     //        contentView.addSubview(darkView)
     //    }
@@ -90,29 +51,9 @@ class TrackCell: UITableViewCell {
         separator.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         separator.heightAnchor.constraint(equalToConstant: 3.0).isActive = true
         separator.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
-
-    func cellClicked() {
-        contentView.addSubview(darkView)
-    }
-    
-    func cellEndedClick() {
-        darkView.removeFromSuperview()
-
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    func setupViews() {
-        contentView.addSubview(separator)
-
-        
-        
-        separator.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        separator.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        separator.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
-        separator.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
-    }
-    
 }
