5cffe21dc74fd3cb1559162226db7b26b1a79eff
MarquelH <marquelhendricks96@gmail.com>
2017-12-23 23:24:56 -0500

    Fonts deleted, launchscreen fixed
    Deleted unnecessary fonts, fixed launchscreen storyboard, and added EventCell (To handle connected outlets) and EventsTableViewController  (To populate table view with DJ's events).

---
+++ DJ APP/DJ APP/EventCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventCell.swift
+//  DJ APP
+//
+//  Created by Marquel Hendricks on 12/23/17.
+//  Copyright © 2017 Marquel and Micajuine App Team. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class EventCell: UITableViewCell {
+    
+    //Will store info for each event via outlets.
+    
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventLocationLabel: UILabel!
+    @IBOutlet weak var eventDateAndTimeLabel: UILabel!
+    
+    
+    
+    
+}
